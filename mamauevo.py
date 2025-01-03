3123f1f70ca4af227142c3f2ed48cb13ef807a04
leamequiell <leanddromero99@gmail.com>
2025-01-03 16:02:38 -0300

    user input num

--- mamauevo.py
+++ mamauevo.py
@@ -1,5 +1,5 @@
-numero1 = 1
-numero2 = 2
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
 
 # Calcular la suma
 resultado = numero1 + numero2
